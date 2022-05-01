498f4a1d40d02759cc43ff3f094ef95fe6b229d2
Bernhard Böck <Bernhard.Boeck@rosenberger.com>
2022-05-01 10:15:52 +0200

    Working, but slow

--- web/fractal.js
+++ web/fractal.js
@@ -1,45 +1,30 @@
 function drawFractal(x1, y1, h, angle) {
   console.log("Draw fractal");
   const canvas = document.getElementById('drawArea');
-  const ctx = canvas.getContext("2d");
+  const context = canvas.getContext("2d");
+  const drawCtx = context.createImageData(320, 200);
 
-  let z = 2;
   let xc = x1;
   let yc = y1;
   let xa = 0;
   let ya = 0;
-  do {
-    const x2 = Math.abs(xa);
-    const y2 = Math.abs(ya);
-    const xn = x2 - y2 - xc;
-    ya = xa * ya;
-    ya = ya + ya - yc;
-    xa = xn;
-    z = z - 0.05;
-    if (z < 0) {
-      break;
-    }
-    if ((x2 + y2)> 400) {
-      break;
-    }
-  } while (true)
 
   angle=angle*Math.PI/180;
   const si=Math.sin(angle);
   const co=Math.cos(angle);
 
-  fillBackground(ctx, canvas.width, canvas.height);
-  drawClouds(ctx);
-  drawLine(ctx,0, 98, 320, 98, getColor(2), 1);
+  fillBackground(drawCtx, canvas.width, canvas.height);
+  drawClouds(drawCtx);
+  drawLine(drawCtx,0, 98, 320, 98, getColor(2));
 
   // draw fractal
   for (let v = 1; v <= 600; v += 2) {
     const e = h / v * 10;
-    let ca = 0;
-    let za = 0;
+    let ca = 0.0;
+    let za = 0.0;
     for (let h0 = -160; h0 <= 160; h0++) {
       let fa = 1;
-      z = 2;
+      let z = 2.0;
       const hc = e * h0 * 0.00935;
       xa = 0;
       ya = 0;
@@ -52,7 +37,7 @@ function drawFractal(x1, y1, h, angle) {
         ya = xa * ya;
         ya = ya + ya - yc;
         xa = xn;
-        z = z - 0.05;
+        z = z - 0.035;
         if (z < 0) {
           break;
         }
@@ -80,35 +65,37 @@ function drawFractal(x1, y1, h, angle) {
       const m0 = 160 + h0;
       const d8 = 98 - c;
       const d9 = 99 - c;
-      drawLine(ctx, m1, 200, m1, d8, getColor(2), 1);
-      drawLine(ctx, m0, 200, m0, d9, getColor(3), 1);
+      drawLine(drawCtx, m1, 200, m1, d8, getColor(2));
+      drawLine(drawCtx, m0, 200, m0, d9, getColor(3));
 
       if (c < 0) {
-        drawPixel(ctx, m0, d9, getColor(fa));
+        drawPixel(drawCtx, m0, d9, getColor(fa));
       }
       if (c > ca + 0.5) {
-        drawLine(ctx, m0, 99 - ca, m0, d9, getColor(0), 1);
+        drawLine(drawCtx, m0, 99 - ca, m0, d9, getColor(0));
       }
-      drawPixel(ctx, m0, d8, getColor(fa));
+      drawPixel(drawCtx, m0, d8, getColor(fa));
       // end of do something
       ca = c;
     }
   }
+
+  context.putImageData(drawCtx, 0, 0)
   console.log("Finished fractal");
 }
 
 function getColor(idx) {
   switch (idx) {
     case 0:
-      return "#00aaaa";
+      return "#00aaaaff";
     case 1:
-      return "#00aa00";
+      return "#00aa00ff";
     case 2:
-      return "#aa0000";
+      return "#aa0000ff";
     case 3:
-      return "#aa5500";
+      return "#aa5500ff";
     default:
-      return "#ffffff";
+      return "#ffffffff";
   }
 }
 
@@ -133,41 +120,45 @@ function drawClouds(ctx) {
       let yb = c1 * j * 0.006;
       let yc = 90 - j;
 
-      drawLine(ctx, xb, yc - c1, xb, yc + yb, "#aa0000", 1);
-      drawPixel(ctx, xb, yc - yb, "#00aaaa")
+      drawLine(ctx, xb, yc - c1, xb, yc + yb, getColor(3));
+      drawPixel(ctx, xb, yc - yb, getColor(0))
     }
   }
 }
 
 function fillBackground(ctx, width, height) {
-  ctx.fillStyle = "#00aaaa";
-  ctx.fillRect(0, 0, width, height);
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      drawPixel(ctx, x, y, getColor(0))
+    }
+  }
 }
 
-function drawLine(ctx, x1, y1, x2, y2, color, width) {
-  ctx.beginPath();
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
-  ctx.strokeStyle = color;
-  ctx.lineWidth = 1;
-  ctx.stroke();
+function drawLine(ctx, rawx1, rawy1, rawx2, rawy2, color) {
+  const x1 = Math.round(rawx1)
+  const x2 = Math.round(rawx2)
+  const y1 = Math.round(rawy1)
+  const y2 = Math.round(rawy2)
+
+  if ( x1 === x2) {
+    // vertical line
+    for (let y = Math.min(y1,y2); y <= Math.max(y1,y2); y++) {
+      drawPixel(ctx, x1, y, color)
+    }
+  } else {
+    // horizontal line
+    for (let x = Math.min(x1, x2); x <= Math.max(x1, x2); x++) {
+      drawPixel(ctx, x, y1, color)
+    }
+  }
 }
 
-/* function drawLine(ctx, x1, y1, x2, y2, color, width) {
-  ctx.beginPath();
-  ctx.moveTo(Math.round(x1) + 0.5, Math.round(y1) + 0.5);
-  ctx.lineTo(Math.round(x2) + 0.5, Math.round(y2) + 0.5);
-  ctx.strokeStyle = color;
-  ctx.lineWidth = 1;
-  ctx.stroke();
-} */
+function drawPixel(ctx, rawx, rawy, color) {
+  const x = Math.round(rawx)
+  const y = Math.round(rawy)
 
-function drawPixel(ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, 1, 1 );
+  ctx.data[x*4 + 320*4*y + 0] = parseInt(color.substring(1,3), 16)
+  ctx.data[x*4 + 320*4*y + 1] = parseInt(color.substring(3,5), 16)
+  ctx.data[x*4 + 320*4*y + 2] = parseInt(color.substring(5,7), 16)
+  ctx.data[x*4 + 320*4*y + 3] = 255
 }
-
-/* function drawPixel(ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(Math.round(x) + 0.5, Math.round(y) + 0.5, 1, 1 );
-} */
