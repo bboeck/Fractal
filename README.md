e2243c2c044306cc581ff94ff3325ff332c92048
Bernhard Böck <Bernhard.Boeck@rosenberger.com>
2022-05-06 20:31:16 +0200

    Performance improvements

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Fractal
-Fun with fractals
+In 1987 a short basic program was published in the issue 7/8'87 of the PC Magazin Plus which draws the appleman in 3D. In 4 colors and full CGA resolution (320x200).
+
+!(https://github.com/bboeck/Fractal/blob/main/Pic1.PNG?raw=true)
+
+I modified the program as a 9 year old and had a lot of fun with it.
+My mother, however, much less, she had to type for me due to lack of own typing skills. 
+Thanks again for that ;)
+On the family XT with 4,7 MHz and 640KB RAM it took about 20 minutes to calculate a picture.
+While cleaning up, I stumbled across this old magazine again and decided to put the program on the web.
--- web/fractal.js
+++ web/fractal.js
@@ -1,11 +1,12 @@
-function drawFractal(x1, y1, h, angle) {
-  console.log("Draw fractal");
+function drawFractal(x1, y1, h, angle, scaleFactor) {
+  console.log(`Draw fractal ${x1} ${y1} ${h} ${angle} ${scaleFactor} ` );
   const canvas = document.getElementById('drawArea');
-  const context = canvas.getContext("2d");
-  const drawCtx = context.createImageData(320, 200);
+  const ctx = canvas.getContext("2d");
 
-  let xc = x1;
-  let yc = y1;
+  // Original CGA resolution.
+  const width = 320
+  const height = 200
+
   let xa = 0;
   let ya = 0;
 
@@ -13,23 +14,23 @@ function drawFractal(x1, y1, h, angle) {
   const si=Math.sin(angle);
   const co=Math.cos(angle);
 
-  fillBackground(drawCtx, canvas.width, canvas.height);
-  drawClouds(drawCtx);
-  drawLine(drawCtx,0, 98, 320, 98, getColor(2));
+  fillBackground(ctx, width, height, scaleFactor);
+  drawClouds(ctx, width, height, scaleFactor);
+  drawLine(ctx,0, height / 2 - 1, width, height / 2 - 1, getColor(2), scaleFactor);
 
   // draw fractal
-  for (let v = 1; v <= 600; v += 2) {
+  for (let v = 1; v <= 200; v += 2) {
     const e = h / v * 10;
     let ca = 0.0;
     let za = 0.0;
-    for (let h0 = -160; h0 <= 160; h0++) {
+    for (let h0 = -width / 2; h0 <= width / 2; h0++) {
       let fa = 1;
       let z = 2.0;
       const hc = e * h0 * 0.00935;
       xa = 0;
       ya = 0;
-      xc = x1 + hc * co + e * si;
-      yc = y1 - hc * si + e * co;
+      let xc = x1 + hc * co + e * si;
+      let yc = y1 - hc * si + e * co;
       do {
         const x2 = Math.abs(xa);
         const y2 = Math.abs(ya);
@@ -46,7 +47,7 @@ function drawFractal(x1, y1, h, angle) {
         }
       } while (true)
 
-      if (h0 > -160) {
+      if (h0 > -width / 2) {
         z = za * 0.7 + z * 0.3;
       }
       if (z < 0) {
@@ -58,29 +59,28 @@ function drawFractal(x1, y1, h, angle) {
       za = z;
 
       // do something
-      if (Math.abs(c) > 102) {
-        c = 102 * Math.sign(c);
+      if (Math.abs(c) > height / 2 + 2) {
+        c = (height / 2 + 2) * Math.sign(c);
       }
-      const m1 = 161 + h0;
-      const m0 = 160 + h0;
-      const d8 = 98 - c;
-      const d9 = 99 - c;
-      drawLine(drawCtx, m1, 200, m1, d8, getColor(2));
-      drawLine(drawCtx, m0, 200, m0, d9, getColor(3));
+      const m1 = width / 2 + 1 + h0;
+      const m0 = width / 2 + h0;
+      const d8 = height / 2 - 2 - c;
+      const d9 = height / 2 - 1 - c;
+      drawLine(ctx, m1, height, m1, d8, getColor(2), scaleFactor);
+      drawLine(ctx, m0, height, m0, d9, getColor(3), scaleFactor);
 
       if (c < 0) {
-        drawPixel(drawCtx, m0, d9, getColor(fa));
+        drawPixel(ctx, m0, d9, getColor(fa), scaleFactor);
       }
       if (c > ca + 0.5) {
-        drawLine(drawCtx, m0, 99 - ca, m0, d9, getColor(0));
+        drawLine(ctx, m0, height / 2 - 1 - ca, m0, d9, getColor(0), scaleFactor);
       }
-      drawPixel(drawCtx, m0, d8, getColor(fa));
+      drawPixel(ctx, m0, d8, getColor(fa), scaleFactor);
       // end of do something
       ca = c;
     }
   }
 
-  context.putImageData(drawCtx, 0, 0)
   console.log("Finished fractal");
 }
 
@@ -99,66 +99,57 @@ function getColor(idx) {
   }
 }
 
-function drawClouds(ctx) {
-  for (let i = 8; i <= 192; i += 4) {
-    let j = Math.pow(i / 192, 6.6);
-    j = j * 90;
+function drawClouds(ctx, width, height, scaleFactor) {
+    for (let i = 8; i <= (height / 2 - 8); i += 4) {
+    let j = Math.pow(i / (height / 2 - 8), 6.6);
+    j = j * (height / 2 * 0.9);
     let br = Math.random() + 0.7;
     br = br * i * .1;
-    let as = Math.random() * Math.random() * 520 - 100;
+    let as = Math.random() * (width + height) - 100;
     let hr = br * (2 - Math.random() * 0.8) * 2;
     for (let h0 = -br; h0 <= br; h0++) {
       let xb = h0 + as;
-      if (Math.abs(xb - 160) > 160) {
+      if (Math.abs(xb - (width / 2)) > (width / 2)) {
         continue;
       }
       let c1 = br * br - h0 * h0;
       c1 = c1 / hr;
-      if (c1 + j > 90) {
-        c1 = 90 - j;
+      if (c1 + j > (height / 2 * 0.9)) {
+        c1 = (height / 2 * 0.9) - j;
       }
       let yb = c1 * j * 0.006;
-      let yc = 90 - j;
+      let yc = (height / 2 * 0.9) - j;
 
-      drawLine(ctx, xb, yc - c1, xb, yc + yb, getColor(3));
-      drawPixel(ctx, xb, yc - yb, getColor(0))
+      drawLine(ctx, xb, yc - c1, xb, yc + yb, getColor(3), scaleFactor);
+      drawPixel(ctx, xb, yc - yb, getColor(0), scaleFactor)
     }
   }
 }
 
-function fillBackground(ctx, width, height) {
+function fillBackground(ctx, width, height, scaleFactor) {
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
-      drawPixel(ctx, x, y, getColor(0))
+      drawPixel(ctx, x, y, getColor(0), scaleFactor)
     }
   }
 }
 
-function drawLine(ctx, rawx1, rawy1, rawx2, rawy2, color) {
-  const x1 = Math.round(rawx1)
-  const x2 = Math.round(rawx2)
-  const y1 = Math.round(rawy1)
-  const y2 = Math.round(rawy2)
-
-  if ( x1 === x2) {
-    // vertical line
-    for (let y = Math.min(y1,y2); y <= Math.max(y1,y2); y++) {
-      drawPixel(ctx, x1, y, color)
-    }
-  } else {
-    // horizontal line
-    for (let x = Math.min(x1, x2); x <= Math.max(x1, x2); x++) {
-      drawPixel(ctx, x, y1, color)
-    }
-  }
+function drawLine(ctx, rawx1, rawy1, rawx2, rawy2, color, scaleFactor) {
+  const x1 = Math.round(rawx1 * scaleFactor)
+  const x2 = Math.round(rawx2 * scaleFactor)
+  const y1 = Math.round(rawy1 * scaleFactor)
+  const y2 = Math.round(rawy2 * scaleFactor)
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2+scaleFactor);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = scaleFactor;
+  ctx.stroke();
 }
 
-function drawPixel(ctx, rawx, rawy, color) {
+function drawPixel(ctx, rawx, rawy, color, scaleFactor) {
   const x = Math.round(rawx)
   const y = Math.round(rawy)
-
-  ctx.data[x*4 + 320*4*y + 0] = parseInt(color.substring(1,3), 16)
-  ctx.data[x*4 + 320*4*y + 1] = parseInt(color.substring(3,5), 16)
-  ctx.data[x*4 + 320*4*y + 2] = parseInt(color.substring(5,7), 16)
-  ctx.data[x*4 + 320*4*y + 3] = 255
+  ctx.fillStyle = color;
+  ctx.fillRect(x * scaleFactor, y * scaleFactor, scaleFactor, scaleFactor);
 }
